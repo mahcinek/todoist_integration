9cf681291126e2a7e9ed4424ab1c9be7efe933e3
Marcin Piwek <marcin.piwek@monterail.com>
2020-02-02 13:10:05 +0100

    Formatted initial commit

--- test/todoist_integration_app_web/views/error_view_test.exs
+++ test/todoist_integration_app_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule TodoistIntegrationWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(TodoistIntegrationWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(TodoistIntegrationWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
